caf352e13fdc4083eb3bdb3c440ab6dd48294233
MahirHamiAbrar <mhabrarmts@gmail.com>
2025-07-11 16:40:12 +0600

    implemented state schemas

--- intellitube_ai_draft.py
+++ intellitube_ai_draft.py
@@ -1,8 +1,16 @@
 import os
 from loguru import logger
-from typing import List
+from pydantic import BaseModel, Field
+from typing_extensions import (
+    Annotated, Sequence, List, Literal, TypedDict, Optional
+)
 
 from langchain_core.documents import Document
+from langchain_core.messages import (
+    AIMessage, SystemMessage, HumanMessage, ToolMessage, BaseMessage
+)
+
+from langgraph.graph.message import add_messages
 
 from intellitube.llm import init_llm
 from intellitube.utils import ChatManager
@@ -45,3 +53,30 @@ document_loader_functions = {
     "youtube_video": document_loader_tools.load_youtube_transcript,
     "website": document_loader_tools.load_webpage
 }
+
+# define Router Agent Output Schema
+class RouterAgentResponse(BaseModel):
+    user_query: str = Field(description=(
+        "The user's original query EXACTLY as it appears, without any modification, rewording, or interpretation.\n"
+        "You MUST NOT include any URLs, file paths, or hyperlinks in this field — only the natural language query.\n"
+        "Preserve the casing, punctuation, and wording. Do NOT fix typos or grammar."
+    ))
+    url: Optional[str] = Field(default=None, description=(
+        "The exact URL or local document/file path mentioned in the user's input.\n"
+        "If there is no URL or file path, leave this as null (do not fabricate one).\n"
+        "Example: 'https://example.com/page', 'C:/Documents/myfile.txt', './notes.md'"
+    ))
+    url_of: Optional[Literal["youtube_video", "website", "document"]] = Field(default=None, description=(
+        "The type of content the `url` field refers to:\n"
+        "- 'youtube_video': if it's a YouTube video link\n"
+        "- 'website': for general websites or web pages\n"
+        "- 'document': for file paths (like .txt, .pdf, .md, etc.)\n"
+        "If no URL/path is provided, this should be null."
+    ))
+
+
+# define Chat Agent Output Schema
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    router_response: Optional[RouterAgentResponse] = None
+    retrieved_docs: Optional[List[Document]] = None
