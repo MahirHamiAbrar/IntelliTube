382f8f3f19093c8b06d7fe622c771a84e2f03487
MahirHamiAbrar <mhabrarmts@gmail.com>
2025-07-15 19:08:38 +0600

    added rest of the nodes and completed the graph

--- agents/summrizer_agent.py
+++ agents/summrizer_agent.py
@@ -1,6 +1,6 @@
 import operator
 from typing import (
-    Annotated, Dict, List, Literal, Optional, TypedDict
+    Any, Annotated, Dict, List, Literal, Optional, TypedDict
 )
 
 from langchain_core.documents import Document
@@ -9,8 +9,13 @@ from langchain_core.language_models import BaseChatModel
 from langchain_core.runnables import RunnableSerializable
 from langchain_core.output_parsers import StrOutputParser
 
+from langchain.chains.combine_documents.reduce import (
+    acollapse_docs, split_list_of_docs
+)
+
 from langgraph.constants import Send
 from langgraph.graph import START, END, StateGraph
+from langgraph.graph.state import CompiledStateGraph
 
 from intellitube.prompts.summarizer_agent_prompts import (
     map_prompt, reduce_prompt
@@ -34,6 +39,8 @@ class SummarizerAgent:
     max_tokens: int
     llm: BaseChatModel
 
+    _graph: StateGraph = None
+    _agent: CompiledStateGraph = None
     _map_chain: RunnableSerializable = None
     _reduce_chain: RunnableSerializable = None
 
@@ -48,6 +55,18 @@ class SummarizerAgent:
         if not self._reduce_chain:
             self._reduce_chain = ChatPromptTemplate([reduce_prompt]) | self.llm | StrOutputParser()
         return self._reduce_chain
+    
+    @property
+    def graph(self) -> StateGraph:
+        if not self._graph:
+            self._graph = self.build_graph()
+        return self._graph
+    
+    @property
+    def agent(self) -> CompiledStateGraph:
+        if not self._agent:
+            self._agent = self.graph.compile()
+        return self._agent
 
     def __init__(self,
         llm: BaseChatModel,
@@ -80,6 +99,51 @@ class SummarizerAgent:
                 for summary in state["summaries"]
             ]
         }
+    
+    async def collapse_summaries(self, state: AgentState) -> Dict[str, list]:
+        """Collapse the collected summaries"""
+        doc_lists = split_list_of_docs(
+            state["collapsed_summaries"], self.length_function, self.token_max
+        )
+        results = []
+        for doc_list in doc_lists:
+            results.append(
+                await acollapse_docs(doc_list, self.reduce_chain.invoke)
+            )
+        return {"collapsed_summaries": results}
+    
+    def should_collapse(self,
+        state: AgentState
+    ) -> Literal["collapse_summaries", "generate_final_summary"]:
+        n_tokens = self.length_function(state["collapsed_summaries"])
+        return (
+            "collapse_summaries"
+            if n_tokens > self.token_max
+            else "generate_final_summary"
+        )
+    
+    async def generate_final_summary(self, state: AgentState) -> Dict[str, Any]:
+        llm_response = await self.reduce_chain.ainvoke(state["collapsed_summaries"])
+        return {"final_summary": llm_response}
+    
+    def build_graph(self) -> StateGraph:
+        graph = (
+            StateGraph(AgentState)
+            # add nodes
+            .add_node("generate_summary", self.generate_summary)  # same as before
+            .add_node("collect_summaries", self.collect_summaries)
+            .add_node("collapse_summaries", self.collapse_summaries)
+            .add_node("generate_final_summary", self.generate_final_summary)
+
+            # add edges
+            .add_conditional_edges(START, self.map_summaries, ["generate_summary"])
+            .add_edge("generate_summary", "collect_summaries")
+            .add_conditional_edges("collect_summaries", self.should_collapse)
+            .add_conditional_edges("collapse_summaries", self.should_collapse)
+            .add_edge("generate_final_summary", END)
+        )
+        self._agent = None  # reset the agent variable
+        return graph
 
 
 
