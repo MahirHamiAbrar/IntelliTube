f89ed96d8c45e8b2d2cc1ace7ae97f8f3230a84a
MahirHamiAbrar <mhabrarmts@gmail.com>
2025-07-16 13:07:57 +0600

    minor modifications

--- agents/base_agent.py
+++ agents/base_agent.py
@@ -1,8 +1,8 @@
-from pathlib import Path
 from typing import Union
+from pathlib import Path
 from abc import ABC, abstractmethod
 
-from langgraph.graph import StateGraph, START, END
+from langgraph.graph import StateGraph
 from langgraph.graph.state import CompiledStateGraph
 from langchain_core.language_models import BaseChatModel
 
--- agents/chat_agent.py
+++ agents/chat_agent.py
@@ -1,11 +1,13 @@
 from langgraph.graph import StateGraph, START, END
+from langchain_core.language_models import BaseChatModel
+
 from intellitube.agents.base_agent import BaseAgent
 
 
 class ChatAgent(BaseAgent):
 
-    def __init__(self) -> None:
-        BaseAgent.__init__(self)
+    def __init__(self, llm: BaseChatModel) -> None:
+        BaseAgent.__init__(self, llm)
     
     def build_graph(self) -> StateGraph:
         pass
