19bca2ebeb3e4843862073d5190763a0821e2c55
MahirHamiAbrar <mhabrarmts@gmail.com>
2025-07-16 22:10:57 +0600

    moved intellitube_ai_draft.py & vector_store_old.py to old_codes dir | created main_agent.py

--- agents/chat_agent.py
+++ agents/chat_agent.py
@@ -1,8 +1,9 @@
-from typing_extensions import Annotated, Sequence, TypedDict
+from typing_extensions import Annotated, List, Sequence, TypedDict
 
 from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph, START, END
 
+from langchain_core.documents import Document
 from langchain_core.messages import BaseMessage
 from langchain_core.language_models import BaseChatModel
 
@@ -13,17 +14,27 @@ from intellitube.agents.base_agent import BaseAgent
 class ChatAgentState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages]
     """Chat conversation messages with the agent"""
+    documents: List[Document] = []
+    """Documents added by the user"""
 
 
 class ChatAgent(BaseAgent):
+    chat_manager: ChatManager
 
     def __init__(self,
         llm: BaseChatModel,
         chat_manager: ChatManager,
     ) -> None:
         BaseAgent.__init__(self, llm)
+        self.chat_manager = chat_manager
     
     def build_graph(self) -> StateGraph:
         graph = StateGraph(state_schema=ChatAgentState)
         super().build_graph()
         return graph
+    
+    def youtube_video_loader_node(self, state: ChatAgentState) -> ChatAgentState:
+        pass
+    
+    def summarizer_node(self, document: Document) -> ChatAgentState:
+        pass
